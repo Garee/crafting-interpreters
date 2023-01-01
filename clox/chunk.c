24e85be74d6051c4a8f11c31cd2d54ac3e1c987b
Gary Blackwood <gary@garyblackwood.co.uk>
2023-01-01 11:11:43 +0000

    feat: debug show line no

--- clox/chunk.c
+++ clox/chunk.c
@@ -6,22 +6,26 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int prevCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(prevCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, prevCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, prevCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
 void freeChunk(Chunk *chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
--- clox/main.c
+++ clox/main.c
@@ -6,11 +6,12 @@ int main() {
     Chunk chunk;
     initChunk(&chunk);
 
+    int line = 123;
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
+    writeChunk(&chunk, OP_CONSTANT, line);
+    writeChunk(&chunk, constant, line);
 
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, line + 1);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
